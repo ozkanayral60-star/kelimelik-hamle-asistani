287cd8d00de121605f9d4ad28fd951e94d622a6a
ozkanayral60-star <ozkanayral60@gmail.com>
2026-09-13 16:00:37 +0300

    Refresh PWA cache for screenshot importer fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kelimelik-asistani-v4';
+const CACHE='kelimelik-asistani-v5';
 const CORE=['./','./index.html','./styles.css','./engine.js','./dictionary.js','./solver-worker.js','./app.js','./screenshot.js','./result-enhancements.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
