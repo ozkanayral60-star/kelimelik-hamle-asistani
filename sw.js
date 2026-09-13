95b793a2adafac8960cf7f2e0a953b0454c5ecb2
ozkanayral60-star <ozkanayral60@gmail.com>
2026-09-13 15:34:15 +0300

    Refresh PWA cache for screenshot import

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE='kelimelik-asistani-v1';
-const CORE=['./','./index.html','./styles.css','./engine.js','./dictionary.js','./solver-worker.js','./app.js','./manifest.webmanifest'];
+const CACHE='kelimelik-asistani-v2';
+const CORE=['./','./index.html','./styles.css','./engine.js','./dictionary.js','./solver-worker.js','./app.js','./screenshot.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+  self.clients.claim()
+])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{if(new URL(e.request.url).origin===location.origin){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return res;}).catch(()=>cached)));
