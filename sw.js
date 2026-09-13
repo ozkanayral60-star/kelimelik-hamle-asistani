5222676e3116e649e94d60a3d192cae4ca1f2327
ozkanayral60-star <ozkanayral60@gmail.com>
2026-09-13 15:38:23 +0300

    Refresh PWA cache for long-word revision

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kelimelik-asistani-v2';
-const CORE=['./','./index.html','./styles.css','./engine.js','./dictionary.js','./solver-worker.js','./app.js','./screenshot.js','./manifest.webmanifest'];
+const CACHE='kelimelik-asistani-v3';
+const CORE=['./','./index.html','./styles.css','./engine.js','./dictionary.js','./solver-worker.js','./app.js','./screenshot.js','./result-enhancements.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
@@ -7,5 +7,5 @@ self.addEventListener('activate',e=>e.waitUntil(Promise.all([
 ])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{if(new URL(e.request.url).origin===location.origin){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return res;}).catch(()=>cached)));
+  e.respondWith(fetch(e.request).then(res=>{if(new URL(e.request.url).origin===location.origin){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return res;}).catch(()=>caches.match(e.request)));
 });
