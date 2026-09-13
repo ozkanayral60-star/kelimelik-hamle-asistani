36ec4fc04b63e14551efcdd30eb8ca4c71367434
ozkanayral60-star <ozkanayral60@gmail.com>
2026-09-13 16:10:11 +0300

    Refresh PWA cache for scanner v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kelimelik-asistani-v5';
-const CORE=['./','./index.html','./styles.css','./engine.js','./dictionary.js','./solver-worker.js','./app.js','./screenshot.js','./result-enhancements.js','./manifest.webmanifest'];
+const CACHE='kelimelik-asistani-v6';
+const CORE=['./','./index.html','./styles.css','./engine.js','./dictionary.js','./solver-worker.js','./dynamic-star.js','./app.js','./screenshot-v2.js','./result-enhancements.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
